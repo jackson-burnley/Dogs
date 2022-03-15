23f0327ec9b713fa4cbf5d067caac37022c9fe2a
Jack <jacktech.system@gmail.com>
2022-03-15 15:33:33 -0300

    Header finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Home";
+import Login from "./Components/Login/Login";
+
 function App() {
   return (
     <div>
-      <h1>App React</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
