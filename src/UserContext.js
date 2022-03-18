b0e73b9767a419dacc26924dcd97c8d3863463a8
Jack <jacktech.system@gmail.com>
2022-03-17 21:45:31 -0300

    Add logout

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TOKEN_POST, USER_GET } from "./api";
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "./api";
 
 export const UserContext = React.createContext();
 
@@ -9,12 +9,41 @@ export const UserStorage = ({ children }) => {
   const [loading, setLoading] = React.useState(null);
   const [error, setError] = React.useState(null);
 
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error("Token Inválido.");
+          await getUser(token);
+        } catch (Error) {
+          console.log("Algo deu errado.");
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, []);
+
   async function getUser(token) {
     const { url, options } = USER_GET(token);
     const userResponse = await fetch(url, options);
     const userJSON = await userResponse.json();
     setData(userJSON);
     setLogin(true);
+  }
+
+  async function userLogout() {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem("token");
   }
 
   async function userLogin(username, password) {
@@ -26,7 +55,7 @@ export const UserStorage = ({ children }) => {
   }
 
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider value={{ userLogin, data, userLogout }}>
       {children}
     </UserContext.Provider>
   );
